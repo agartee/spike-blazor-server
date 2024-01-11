fa765ecb165588d6c0d617c46f0c17733242991c
Adam Gartee <agartee@gmail.com>
2024-01-11 10:30:49 -0500

    readding test file

---
+++ test/Spike.WebApp.Tests/Pages/HomeTests.cs
@@ -0,0 +1,39 @@
+﻿using Bunit;
+using Bunit.TestDoubles;
+using FluentAssertions;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.Extensions.DependencyInjection;
+using Moq;
+using Spike.WebApp.Pages;
+
+namespace Spike.WebApp.Tests.Pages
+{
+    public class HomeTests : TestContext
+    {
+        // reference:
+        // https://learn.microsoft.com/en-us/aspnet/core/blazor/test?view=aspnetcore-8.0
+        // https://bunit.dev/docs/providing-input/inject-services-into-components.html
+        // https://bunit.dev/api/Bunit.TestDoubles.FakeAuthorizationPolicyProvider.html
+
+        private readonly Mock<IMediator> mediator = new Mock<IMediator>();
+        private readonly IAuthorizationPolicyProvider authPolicyProvider = new FakeAuthorizationPolicyProvider();
+
+        public HomeTests()
+        {
+            Services.AddSingleton(mediator.Object);
+            Services.AddSingleton(authPolicyProvider);
+        }
+
+        [Fact]
+        public void OnInit_RendersHeader()
+        {
+            var component = RenderComponent<Home>();
+
+            component.Find("h1")
+                .MarkupMatches("<h1>Hello, world!</h1>");
+
+            component.FindAll("h3").Should().BeEmpty();
+        }
+    }
+}
